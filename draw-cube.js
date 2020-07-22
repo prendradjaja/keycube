8c6cffc12f898e85b0a834769651f92613f94400
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-07-21 23:09:31 -0700

    IT WORKS

--- draw-cube.js
+++ draw-cube.js
@@ -1,33 +1,67 @@
+const template = `
+               +--------+--------+--------+
+             .'1111111.'2222222.'3333333.'|
+           .'1111111.'2222222.'3333333.'CC|
+          +--------+--------+--------+CCCC|
+        .'4444444.'5555555.'6666666.'|CCCC|
+      .'4444444.'5555555.'6666666.'BB|CCCC+
+     +--------+--------+--------+BBBB|CC.'|
+   .'7777777.'8888888.'9999999.'|BBBB|.'FF|
+ .'7777777.'8888888.'9999999.'AA|BBBB+FFFF|
++--------+--------+--------+AAAA|BB.'|FFFF|
+|aaaaaaaa|bbbbbbbb|cccccccc|AAAA|.'EE|FFFF+
+|aaaaaaaa|bbbbbbbb|cccccccc|AAAA+EEEE|FF.'|
+|aaaaaaaa|bbbbbbbb|cccccccc|AA.'|EEEE|.'II|
+|aaaaaaaa|bbbbbbbb|cccccccc|.'DD|EEEE+IIII|
++--------+--------+--------+DDDD|EE.'|IIII|
+|dddddddd|eeeeeeee|ffffffff|DDDD|.'HH|IIII+
+|dddddddd|eeeeeeee|ffffffff|DDDD+HHHH|II.' 
+|dddddddd|eeeeeeee|ffffffff|DD.'|HHHH|.'   
+|dddddddd|eeeeeeee|ffffffff|.'GG|HHHH+     
++--------+--------+--------+GGGG|HH.'      
+|gggggggg|hhhhhhhh|iiiiiiii|GGGG|.'        
+|gggggggg|hhhhhhhh|iiiiiiii|GGGG+          
+|gggggggg|hhhhhhhh|iiiiiiii|GG.'           
+|gggggggg|hhhhhhhh|iiiiiiii|.'             
++--------+--------+--------+               
+`;
+
 const stickerSize = 100;
 
 function draw(cube) {
-  document.querySelectorAll('.cube')
-    .forEach(cubeEl => {
-      setAngleClass(cubeEl);
-      ['f', 'r', 'u', 'l'].forEach(face =>
-        cubeEl.querySelector(`.${face}-face`).innerHTML = (() => {
-          let result = '';
-          for (let r = 0; r < 3; r++) {
-            for (let c = 0; c < 3; c++) {
-              result += `
-                <rect
-                  x="${c * stickerSize}"
-                  y="${r * stickerSize}"
-                  height="100"
-                  width="100"
-                  fill="${getColor(cube, face, r, c)}"
-                  stroke="black"
-                  stroke-width="2"
-                />
-              `;
-              // TODO Bigger stroke width (requires some more
-              // math bc this is not "box-sizing: border-box")
-            }
-          }
-          return result;
-        })()
-      );
-    });
+  let text = template;
+
+  text = text.replace(/1/g, getColor(cube, 'u', 0, 0));
+  text = text.replace(/2/g, getColor(cube, 'u', 0, 1));
+  text = text.replace(/3/g, getColor(cube, 'u', 0, 2));
+  text = text.replace(/4/g, getColor(cube, 'u', 1, 0));
+  text = text.replace(/5/g, getColor(cube, 'u', 1, 1));
+  text = text.replace(/6/g, getColor(cube, 'u', 1, 2));
+  text = text.replace(/7/g, getColor(cube, 'u', 2, 0));
+  text = text.replace(/8/g, getColor(cube, 'u', 2, 1));
+  text = text.replace(/9/g, getColor(cube, 'u', 2, 2));
+
+  text = text.replace(/a/g, getColor(cube, 'f', 0, 0));
+  text = text.replace(/b/g, getColor(cube, 'f', 0, 1));
+  text = text.replace(/c/g, getColor(cube, 'f', 0, 2));
+  text = text.replace(/d/g, getColor(cube, 'f', 1, 0));
+  text = text.replace(/e/g, getColor(cube, 'f', 1, 1));
+  text = text.replace(/f/g, getColor(cube, 'f', 1, 2));
+  text = text.replace(/g/g, getColor(cube, 'f', 2, 0));
+  text = text.replace(/h/g, getColor(cube, 'f', 2, 1));
+  text = text.replace(/i/g, getColor(cube, 'f', 2, 2));
+
+  text = text.replace(/A/g, getColor(cube, 'r', 0, 0));
+  text = text.replace(/B/g, getColor(cube, 'r', 0, 1));
+  text = text.replace(/C/g, getColor(cube, 'r', 0, 2));
+  text = text.replace(/D/g, getColor(cube, 'r', 1, 0));
+  text = text.replace(/E/g, getColor(cube, 'r', 1, 1));
+  text = text.replace(/F/g, getColor(cube, 'r', 1, 2));
+  text = text.replace(/G/g, getColor(cube, 'r', 2, 0));
+  text = text.replace(/H/g, getColor(cube, 'r', 2, 1));
+  text = text.replace(/I/g, getColor(cube, 'r', 2, 2));
+
+  document.querySelector('#ascii-cube').innerHTML = text;
 }
 
 function getColor(cube, faceName, r, c) {
@@ -37,12 +71,12 @@ function getColor(cube, faceName, r, c) {
 
 function stickerToColor(sticker) {
   return {
-    U: 'white',
-    F: 'rgb(0, 200, 0)',
-    R: 'rgb(200, 0, 0)',
-    B: 'rgb(0, 0, 200)',
-    L: 'rgb(200, 100, 0)',
-    D: 'rgb(200, 200, 0)',
+    U: ' ',
+    D: '@',
+    F: '.',
+    B: '*',
+    L: '{',
+    R: '&',
   }[sticker];
 }
 
