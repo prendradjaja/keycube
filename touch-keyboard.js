078f48c0663cd142eb33900e4f8308da45e6ab40
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-01-26 21:42:47 -0800

    Clickable buttons

---
+++ touch-keyboard.js
@@ -0,0 +1,36 @@
+const touchKeyboardEl = document.getElementById('touch-keyboard');
+
+const layout = `
+
+  x  B' B  y
+  L' U' U  R
+  L  F' F  R'
+  y' D  D' x'
+
+`.trim().split('\n').map(line => line.trim().split(/\s+/))
+console.log(layout)
+
+let keyboardHtml = '';
+for (let row of layout) {
+  keyboardHtml += '<tr>';
+  for (let key of row) {
+    keyboardHtml += `
+      <td>
+        <button
+          onclick="handleButtonClick(this, &quot;${key}&quot;)"
+        >
+          ${key}
+        </button>
+      </td>
+    `;
+  }
+  keyboardHtml += '</tr>';
+}
+touchKeyboardEl.innerHTML = keyboardHtml;
+
+function handleButtonClick(button, move) {
+  button.classList.add('active');
+  setTimeout(() => {
+    button.classList.remove('active');
+  }, 500);
+}
