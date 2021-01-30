37de51c3c6b12bf0f3b29cad02622d0dc3dba480
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-01-30 15:19:52 -0800

    Modifier keys work now I think

--- touch-keyboard.js
+++ touch-keyboard.js
@@ -1,40 +1,84 @@
 const touchKeyboardEl = document.getElementById('touch-keyboard');
 
-let layout = `
+class touchKeyboard {
+  layers = undefined; // will be initialized below
+  activeLayerIndex = 0;
+  getActiveLayer() {
+    return this.layers[this.activeLayerIndex];
+  }
+  // TODO bring all the stuff from this file into this class
+}
+touchKeyboard = new touchKeyboard()
+
+touchKeyboard.layers = `
 
   y' B  B' y
   L' U' U  R
   L  F' F  R'
   x' D  D' x
 
-  a b c
-  d e f
+  y' b  b' y
+  l' u' u  r
+  l  f' f  r'
+  x' d  d' x
+
+  _  _  _  _
+  M' _  _  M'
+  M  _  _  M
+  z' _  _  z
 
 `.split('\n\n').map(chunk => chunk.trim()).filter(chunk => chunk).map(chunk => chunk.split('\n').map(line => line.trim().split(/\s+/)))
-layout = layout[0];
 
-let keyboardHtml = '';
-for (let row of layout) {
-  keyboardHtml += '<tr>';
-  for (let key of row) {
-    keyboardHtml += `
-      <td>
-        <button
-          ontouchstart="handleButtonClick(this, &quot;${key}&quot;)"
-        >
-          ${key}
-        </button>
-      </td>
-    `;
+createTouchKeyboard();
+
+function createTouchKeyboard() {
+  let keyboardHtml = '';
+  for (let [r, row] of touchKeyboard.getActiveLayer().entries()) {
+    keyboardHtml += '<tr>';
+    for (let [c, key] of row.entries()) {
+      keyboardHtml += `
+        <td>
+          <button
+            id="touch-keyboard-button-${r}-${c}"
+            onclick="handleButtonClick(this, ${r}, ${c})"
+          >
+          </button>
+        </td>
+      `;
+    }
+    keyboardHtml += '</tr>';
   }
-  keyboardHtml += '</tr>';
+  touchKeyboardEl.innerHTML = keyboardHtml;
+  activateLayer(0);
 }
-touchKeyboardEl.innerHTML = keyboardHtml;
 
-function handleButtonClick(button, move) {
+function handleButtonClick(button, r, c) {
+  const { move } = getBinding(r, c);
   handleMove(move);
+
   button.classList.add('active');
-  setTimeout(() => {
+  setTimeout(() => { // TODO maybe do this on touchend
     button.classList.remove('active');
   }, 200);
+}
+
+function activateLayer(i) {
+  touchKeyboard.activeLayerIndex = i;
+  for (let [r, row] of touchKeyboard.getActiveLayer().entries()) {
+    for (let [c, key] of row.entries()) {
+      const buttonEl = document.getElementById(`touch-keyboard-button-${r}-${c}`);
+      const { move, fallThrough } = getBinding(r, c);
+      buttonEl.textContent = move;
+    }
+  }
+}
+
+function getBinding(r, c) {
+  let move = touchKeyboard.getActiveLayer()[r][c];
+  let fallThrough = false;
+  if (move === '_') {
+    move = touchKeyboard.layers[0][r][c];
+    fallThrough = true;
+  }
+  return { move, fallThrough };
 }
