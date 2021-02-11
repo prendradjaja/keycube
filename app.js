f43f90a549093c347304b5a672aae89fe4b965c9
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-02-10 17:44:51 -0800

    CROSS SOLVED

--- app.js
+++ app.js
@@ -52,10 +52,25 @@ function handleMove(move) {
   draw(cube);
 }
 
+const crossSolvedScrambles = `D' F2 L2 U F2 D U2 F2 R2 U R' U' L2 R2 B L2 B' U2 R' U
+D F2 R2 U2 R F B2 U' R' F2 U' D' F2 B2 D B2 L2 U' L2 U
+U' D2 L B2 F2 D2 B2 R' F2 L2 R D' B U' B' F2 D' R'
+R2 D R2 F' U2 B' D2 L2 B2 L2 F U2 R' F2 D L U R D L2
+R' U2 F2 L2 B U2 R2 F U2 R2 F2 U2 R D F L' D' L2 B2 L
+D L D2 B2 U R2 B2 L2 F2 U2 L2 U L2 R F U' L2 F R B' L
+D2 F U2 F L2 F' R2 F' L2 R2 U2 D' L' B' U' B' L B2 D'
+U' R B2 D' R2 D2 U' B2 U' B2 F2 U' L' R2 U B D2 R' F2
+F2 L' B R2 D2 U2 F' L2 R2 D2 B' L2 U2 L' B2 L2 F2 U' F U2`.split('\n')
+
+// https://stackoverflow.com/a/4550514
+function randomChoice(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
 function scramble() {
-  cube.init(Cube.random());
-  // cube.init(new Cube());
-  // cube.move('R');
+  // cube.init(Cube.random());
+  cube.init(new Cube());
+  cube.move(randomChoice(crossSolvedScrambles));
 
   draw(cube);
   globalState.state = INSPECTION;
