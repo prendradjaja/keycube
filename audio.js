14cb5cdbd84113b0b7a1cfcec40f2b5a62677d4f
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-02-17 21:07:15 -0800

    oops add audio.js

---
+++ audio.js
@@ -0,0 +1,19 @@
+var audioContext = new (window.AudioContext || window.webkitAudioContext)();
+//  ( "Webkit/blink browsers need prefix, Safari won't work without window.")
+
+var masterGain = audioContext.createGain();
+    masterGain.gain.value = 0.5;
+masterGain.connect(audioContext.destination);
+
+var nodeGain1 = audioContext.createGain();
+    nodeGain1.gain.value = 0.5;
+nodeGain1.connect(masterGain);
+
+function clickBuzz( frequency, length) {
+    var oscillatorNode = new OscillatorNode(audioContext, {type: 'square'});
+    oscillatorNode.frequency.value = frequency;
+    oscillatorNode.connect( nodeGain1);
+    oscillatorNode.start(audioContext.currentTime);
+    //setTimeout( function(){oscillatorNode.stop();}, length*1000);
+    oscillatorNode.stop( audioContext.currentTime + length);
+}
